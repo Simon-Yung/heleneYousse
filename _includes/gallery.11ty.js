6e7b93d81835f1610998f5c074089da7a74b8437
SimonYung <simonquere24@gmail.com>
2022-01-21 00:20:36 +0100

    created a sorting template for galleries, settings files and images for collections in _data folder

---
+++ _includes/gallery.11ty.js
@@ -0,0 +1,81 @@
+exports.data = {
+	layout : "outerGallery.njk"
+};
+const Image = require("@11ty/eleventy-img");
+function resizeImage( file ) {
+
+	// generate a new jpeg image with 300px width and copy it and the og image to the /img/ folder
+	Image( file, {
+		widths: [ 300 , null ],
+		formats: [ "jpeg" ],
+		urlPath: "/img/",
+		outputDir: "./img/"
+	});
+	// get the metadata of the image even if the image generation is not finished yet
+	let metadata = Image.statsSync( file, {
+		widths: [ 300 , null ],
+		formats: [ "jpeg" ],
+		urlPath: "/img/",
+		outputDir: "./img/"
+	});
+
+	return {
+	thumbnailUrl : metadata.jpeg[0].url,
+	OGUrl : metadata.jpeg[metadata.jpeg.length - 1].url, // <---- metadata.jpeg[1] returns undefined ?!?!
+	heightRatio : metadata.jpeg[0].height / metadata.jpeg[0].width * 100, // height is ??? percent of the width
+	widthRatio : metadata.jpeg[0].height / metadata.jpeg[0].width * 100 // width is ??? percent of the height
+	}
+}
+exports.render = function(data) {
+
+	let columns = [];
+	for (i=0; i < data.number_of_columns; i++){
+		columns.push( { size : 0, imagesInColumn : []} );
+	}
+
+	for (i=0; i < data.imageCollections[data.collection_id].info.imagesList.length; i++ ){
+
+		let smallestColumn = 0;
+		let smallestColumnSize = columns[0].size;
+
+		for (ii=1; ii < columns.length; ii++){
+			if (columns[ii].size < smallestColumnSize) {
+				smallestColumnSize = columns[ii].size;
+				smallestColumn = ii;
+			};
+		}
+
+		columns[smallestColumn].imagesInColumn.unshift({
+			...resizeImage( `./_data/imageCollections/${data.collection_id}/${data.imageCollections[data.collection_id].info.imagesList[i].file}` ),
+			...{ id : i, caption : data.imageCollections[data.collection_id].info.imagesList[i].caption }
+		})
+
+		columns[smallestColumn].size = columns[smallestColumn].size + columns[smallestColumn].imagesInColumn[0].heightRatio;
+	}
+
+	let gallery = `<div class="gallery">
+	`;
+	for (i=0; i < columns.length; i++ ){
+		gallery = gallery + `<div class="gallery__column">
+		`;
+		for (ii=0; ii < columns[i].imagesInColumn.length; ii++ ){
+			gallery = gallery + `<div class="preview__widthBox">
+				<div class="preview__widthBox_heightBox" style="padding-bottom:${columns[i].imagesInColumn[ii].heightRatio}%">
+					<img class="preview__image" 
+					id=""${columns[i].imagesInColumn[ii].id}
+					src="${columns[i].imagesInColumn[ii].thumbnailUrl}"
+					data-OGUrl="${columns[i].imagesInColumn[ii].thumbnailUrl}"
+					data-caption="${columns[i].imagesInColumn[ii].caption}"
+					alt="${columns[i].imagesInColumn[ii].caption}"
+					loading="lazy">
+				</div>
+			</div>
+			`;
+		}
+		gallery= gallery + `</div>
+		`;
+	}
+	gallery = gallery + `</div`;
+
+	return gallery;
+	};
---
+++ galleryBronzes.md
@@ -0,0 +1,12 @@
+---
+layout: gallery.11ty.js
+title: Gallery Bronzes
+name: Gallery Bronzes
+description:
+
+collection_id: bronzes
+number_of_columns: 4
+lang: fr
+tags: fr
+---
+
