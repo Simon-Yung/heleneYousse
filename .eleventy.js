e355c712a6f2bc24f62ff3cc8c081df375efbfd2
SimonYung <simonquere24@gmail.com>
2022-01-20 14:44:07 +0100

    added support for .csv and .yaml data files

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,43 @@
-module.exports = function(eleventyConfig) {
+const yaml = require("js-yaml");
+const csv = require("papaparse");
+
+module.exports = (eleventyConfig) => {
 
 	//pass through copy for css javascript and internal images
 	eleventyConfig.addPassthroughCopy({ "_includes/assets": "assets" });
 	eleventyConfig.addPassthroughCopy({ "favicon.ico": "favicon.ico" });
 	eleventyConfig.addPassthroughCopy({ "favicon.png": "favicon.png" });
 
+	eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+	eleventyConfig.addDataExtension(
+		"csv",
+		(contents) => {
+			// console.log(contents);
+			// console.log(typeof(contents)); //content is a string
+			return csv.parse( 
+				contents ,
+				{
+					// see https://www.papaparse.com/docs#config for detailed config options
+					delimiter: ",",
+					newline: "",// auto-detect
+					quoteChar: '"',
+					escapeChar: '\\',
+					header: true,
+					encoding: "UTF-8",
+					complete: ( result, file ) => {
+						if (result.errors){
+							for ( let i=0 ; i < result.errors.length ; i++ )
+							console.log(result.errors[i])
+						}
+						console.log( result.data ); // for debug purpose
+						return result.data;
+					},
+					error: ( error, file ) => { console.log(error) },
+					download: false,
+					skipEmptyLines: "greedy",
+					fastMode: undefined, //Fast mode will automatically be enabled if no " characters appear in the input. 
+				}
+			);
+		}
+	);
 };
